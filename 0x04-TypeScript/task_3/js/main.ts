94f933c3bbad4c1dbc25768b017478e4498ca562
Emnet Befkadu <162457277+Emnet19@users.noreply.github.com>
2025-09-14 22:40:36 +0300

    Update main.ts

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -3,23 +3,15 @@
 import { RowID, RowElement } from './interface';
 import * as CRUD from './crud.js';
 
-// Create a row object
 const row: RowElement = {
   firstName: "Guillaume",
   lastName: "Salva",
 };
 
-// Insert the row and get the new row ID
 const newRowID: RowID = CRUD.insertRow(row);
 
-// Update the row with an age field
-const updatedRow: RowElement = {
-  ...row,
-  age: 23,
-};
+// Updated row using spread syntax
+const updatedRow: RowElement = { ...row, age: 23 };
 
-// Call updateRow
 CRUD.updateRow(newRowID, updatedRow);
-
-// Call deleteRow
 CRUD.deleteRow(newRowID);
