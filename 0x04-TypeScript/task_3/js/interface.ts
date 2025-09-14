7d6bc06fda88d4d449ca02369ae56e173f145284
Emnet Befkadu <162457277+Emnet19@users.noreply.github.com>
2025-09-14 22:32:36 +0300

    Create interface.ts

---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,9 @@
+// Define a type for Row ID
+export type RowID = number;
+
+// Define an interface for a row element
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number;
+}
