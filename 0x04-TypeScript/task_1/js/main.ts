5f609fa50f988682ede60db6b324a992ddfaa9c3
Emnet Befkadu <162457277+Emnet19@users.noreply.github.com>
2025-09-21 20:52:06 +0300

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -16,3 +16,28 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;  s
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number; 
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
