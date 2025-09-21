53dc69af49cf5bd01544e8b3b62195169ae545c6
Emnet Befkadu <162457277+Emnet19@users.noreply.github.com>
2025-09-21 21:07:11 +0300

    Update crud.d.ts

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -2,8 +2,6 @@
 
 import { RowID, RowElement } from './interface';
 
-// Type declarations for the CRUD functions using the correct types
-export function insertRow(row: RowElement): RowID;
-export function deleteRow(rowId: RowID): void;
-export function updateRow(rowId: RowID, row: RowElement): RowID;
-
+declare function insertRow(row: RowElement): RowID;
+declare function deleteRow(rowId: RowID): void;
+declare function updateRow(rowId: RowID, row: RowElement): RowID;
